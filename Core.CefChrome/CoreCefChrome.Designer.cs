37fa928bb69007f8dcc59d78fbed6d0f5ebff6c1
shizheng <shizheng@jizhibao.com.cn>
2021-09-02 18:02:57 +0800

    添加cefchrome

---
+++ Core.CefChrome/CoreCefChrome.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace Core.CefChrome
+{
+    partial class CoreCefChrome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ptop = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.BtnGetCookie = new System.Windows.Forms.Button();
+            this.ptop.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ptop
+            // 
+            this.ptop.Controls.Add(this.BtnGetCookie);
+            this.ptop.Controls.Add(this.btnGo);
+            this.ptop.Controls.Add(this.textBox1);
+            this.ptop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ptop.Location = new System.Drawing.Point(0, 0);
+            this.ptop.Name = "ptop";
+            this.ptop.Size = new System.Drawing.Size(1044, 67);
+            this.ptop.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(941, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(959, 12);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 1;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // BtnGetCookie
+            // 
+            this.BtnGetCookie.Location = new System.Drawing.Point(878, 39);
+            this.BtnGetCookie.Name = "BtnGetCookie";
+            this.BtnGetCookie.Size = new System.Drawing.Size(75, 23);
+            this.BtnGetCookie.TabIndex = 2;
+            this.BtnGetCookie.Text = "GetCookie";
+            this.BtnGetCookie.UseVisualStyleBackColor = true;
+            this.BtnGetCookie.Click += new System.EventHandler(this.BtnGetCookie_Click);
+            // 
+            // CoreCefChrome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1044, 774);
+            this.Controls.Add(this.ptop);
+            this.Name = "CoreCefChrome";
+            this.Text = "CoreCefChrome";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.CoreCefChrome_FormClosing);
+            this.ptop.ResumeLayout(false);
+            this.ptop.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel ptop;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button BtnGetCookie;
+    }
+}
+
---
+++ Core.CefChrome/CoreCefChrome.cs
@@ -0,0 +1,107 @@
+﻿using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using System.Net;
+using CefSharp.DevTools.Network;
+
+namespace Core.CefChrome
+{
+    public partial class CoreCefChrome : Form
+    {
+
+        public ChromiumWebBrowser chromiumWebBrowser;
+
+        public CoreCefChrome()
+        {
+            InitializeComponent();
+
+            InitChrome();
+        }
+
+        public void InitChrome()
+        {
+            CefSettings settings = new CefSettings();
+
+            Cef.Initialize(settings);
+
+            chromiumWebBrowser = new ChromiumWebBrowser("http://vip337.com:8866");
+            chromiumWebBrowser.FrameLoadEnd += ChromiumWebBrowser_FrameLoadEnd;
+            this.Controls.Add(chromiumWebBrowser);
+
+            chromiumWebBrowser.Dock = DockStyle.Fill;
+        }
+
+        private void ChromiumWebBrowser_FrameLoadEnd(object sender, FrameLoadEndEventArgs e)
+        {
+            chromiumWebBrowser.GetBrowser().MainFrame.ExecuteJavaScriptAsync("document.getElementById('username').value='shizhengzl'");
+            chromiumWebBrowser.GetBrowser().MainFrame.ExecuteJavaScriptAsync("document.getElementById('passwd').value='shizi120'");
+        }
+
+        private void CoreCefChrome_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            var list = chromiumWebBrowser.GetBrowser().GetFrameNames();
+            
+            list.ToList().ForEach(x=>{
+                chromiumWebBrowser.GetBrowser().GetFrame(x).ExecuteJavaScriptAsync("document.getElementById('username').value='shizhengzl'");
+                chromiumWebBrowser.GetBrowser().GetFrame(x).ExecuteJavaScriptAsync("document.getElementById('passwd').value='shizi120'");
+            });
+           
+        }
+
+        private void BtnGetCookie_Click(object sender, EventArgs e)
+        { 
+            //注册获取cookie回调事件
+            ICookieManager cookieManager = chromiumWebBrowser.GetCookieManager();
+            CookieVisitor visitor = new CookieVisitor();
+            visitor.SendCookie += SaveCookie;
+            cookieManager.VisitAllCookies(visitor);
+
+            StringBuilder sb = new StringBuilder();
+            cookies.ForEach(cookie => { 
+                sb.AppendFormat("{0}:{1}",cookie.Name,cookie.Value);
+                sb.AppendLine();
+            });
+           
+            MessageBox.Show(sb.ToString());
+        }
+
+        List<CefSharp.Cookie> cookies = new List<CefSharp.Cookie>();
+
+        private void SaveCookie(CefSharp.Cookie cookie)
+        {
+            if(!cookies.Any(x=>x.Name == cookie.Name && x.Value == cookie.Value))
+                cookies.Add(cookie); 
+        }
+    }
+
+    public class CookieVisitor : ICookieVisitor
+    {
+        //定义委托
+        public Action<CefSharp.Cookie> SendCookie = null;
+        public bool Visit(CefSharp.Cookie cookie, int count, int total, ref bool deleteCookie)
+        {
+            if (SendCookie != null)
+            {
+                SendCookie(cookie);
+            }
+
+            return true;
+        }
+
+        public void Dispose() { }
+    }
+
+}
